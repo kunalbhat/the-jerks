98eadfdca6a2000b88c39700f8c00013de298739
Kunal Bhat <kunalbhat@gmail.com>
2016-07-31 17:53:35 -0500

    Writes to the database

--- models/post.rb
+++ models/post.rb
@@ -5,14 +5,15 @@ class Post
   property :tmdb_id       , Integer
   property :title         , String
   property :director_name , String
+  property :certification , String
   property :release_date  , Date
-  property :runtime       , Integer
-  property :j_seen_bool   , Boolean
-  property :e_seen_bool   , Boolean
-  property :r_seen_bool   , Boolean
-  property :k_seen_bool   , Boolean
-  property :s_seen_bool   , Boolean
-  property :removal_flag  , Boolean
+  property :runtime       , String
+  property :j_seen_bool   , String
+  property :e_seen_bool   , String
+  property :r_seen_bool   , String
+  property :k_seen_bool   , String
+  property :s_seen_bool   , String
+  property :removal_flag  , String
 
   DataMapper.auto_migrate!
   DataMapper.finalize.auto_upgrade!
