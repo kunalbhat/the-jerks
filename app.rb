e6e177d674180322fdc170c89106f81312f38c82
Kunal Bhat <kunalbhat@gmail.com>
2016-07-31 14:13:26 -0500

    Start working on importing csv

--- app.rb
+++ app.rb
@@ -1,19 +1,65 @@
 require 'bundler/setup'
+require 'csv'
+require 'rest_client'
+require 'json'
 
 Bundler.require :web
 Bundler.require :development if development?
 Dotenv.load
 
-require_relative 'config/tmdb'
-
+# Configure Sass
 get '/style.css' do
   scss :stylesheet, :style => :expanded
 end
 
-get '/' do
-  haml :index
+# Search TMDB for a movie, return results
+def get_result(title)
+  @title = title
+
+  headers = {
+    :content_type => 'json',
+    :accept => 'json',
+    :api_key => ENV['TMDB_API_KEY'],
+    :query => @title
+  }
+
+  response = RestClient.get "http://api.themoviedb.org/3/search/movie", {:params => headers }
+
+  json_response = JSON.parse(response)
+
+  return json_response
 end
 
-url = "#{@tmdb_api_base_url}movie/550?api_key=#{ENV['TMDB_API_KEY']}"
+def read_csv_into_array
+  csv_titles = []
 
-p url
+  CSV.foreach("list.csv", :headers => true) do |row|
+    hash = { :title => row[1], :year => row[0] }
+    csv_titles.push(hash)
+  end
+
+  return csv_titles
+end
+
+get '/' do
+  titles = read_csv_into_array
+  @films = []
+
+  titles[1..5].each do |title|
+    title.each
+  end
+
+  # titles[1..5].each do |title|
+  #   year = title['year']
+  #   title = title['title']
+
+  #   results = get_result(title)
+
+  #   results['results'].each do |result|
+  #     hash = { :title => result['title'], :year => result['release_date'], :id => result['id'] }
+  #     @films.push(hash)
+  #   end
+  # end
+
+  haml :index
+end
