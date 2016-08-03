4f9cefc37b835781f6de42d7cfeec77516137a56
Kunal Bhat <kunalbhat@gmail.com>
2016-08-02 23:27:46 -0500

    Display some data on the film detail page

--- app.rb
+++ app.rb
@@ -28,21 +28,27 @@ def configuration
     api_key:  ENV['TMDB_API_KEY']
   }
 
-  response     = RestClient.get "https://api.themoviedb.org/3/configuration", { params: headers }
-  json_reponse = JSON.parse(response)
+  response      = RestClient.get "https://api.themoviedb.org/3/configuration", { params: headers }
+  json_response = JSON.parse(response)
 
   return json_response
+end
 
+def build_image_url(poster_size, poster_path)
+  config_data = configuration
+  base_url    = config_data['images']['base_url']
+  image_url   = "#{base_url}#{poster_size}#{poster_path}"
+
+  return image_url
 end
 
 # Search TMDB for a movie, return results
 def search_movie(title)
   headers = {
-    accept:              'application/json',
-    api_key:             ENV['TMDB_API_KEY'],
-    append_to_response:  'configuration'
-    content_type:        'json',
-    query:               title,
+    accept:        'application/json',
+    api_key:       ENV['TMDB_API_KEY'],
+    content_type:  'json',
+    query:         title,
   }
 
   response      = RestClient.get "https://api.themoviedb.org/3/search/movie/", { params: headers }
@@ -53,8 +59,9 @@ end
 
 def get_movie_by_id(id)
   headers = {
-    accept:  'application/json',
-    api_key: ENV['TMDB_API_KEY']
+    accept:              'application/json',
+    api_key:             ENV['TMDB_API_KEY'],
+    append_to_response:  'release_dates'
   }
 
   response      = RestClient.get "https://api.themoviedb.org/3/movie/#{id}", { params: headers }
@@ -83,11 +90,22 @@ get '/search' do
 end
 
 get '/movie/:id' do
-  id = params[:id]
+  id          = params[:id]
+  film_data   = get_movie_by_id(id)
 
-  @film_data = get_movie_by_id(id)
+  # Get poster
+  poster_size = 'w342'
+  poster_path = film_data['poster_path']
+  image_url   = build_image_url(poster_size, poster_path)
 
-  { :data => @film_data }.to_json
+  # Get film details
+  title         = film_data['title']
+  overview      = film_data['overview']
+  year          = DateTime.parse(film_data['release_date']).year
+
+  p film_data
+
+  @film_data = { title: title, overview: overview, year: year, image_url: image_url }
 
   haml :movie
 end
