04a1cfddeb30f9a1cbc7f68eb4e449af65ed9824
Kunal Bhat <kunalbhat@gmail.com>
2016-08-02 21:40:33 -0500

    Start building out film detail page

--- app.rb
+++ app.rb
@@ -20,20 +20,44 @@ get '/stylesheet.css' do
   scss :stylesheet, :style => :expanded
 end
 
-# Search TMDB for a movie, return results
-def search_movie(title)
-  @title = title
-
+# Check configuration endpoint
+# TODO: Need to cache this data and perodically check for updates (days)
+def configuration
   headers = {
-    content_type:        'json',
-    accept:              'json',
-    api_key:             ENV['TMDB_API_KEY'],
-    query:               @title,
-    append_to_response:  'configuration'
+    accept:   'application/json',
+    api_key:  ENV['TMDB_API_KEY']
   }
 
-  response = RestClient.get "https://api.themoviedb.org/3/search/movie/", { :params => headers }
+  response     = RestClient.get "https://api.themoviedb.org/3/configuration", { params: headers }
+  json_reponse = JSON.parse(response)
 
+  return json_response
+
+end
+
+# Search TMDB for a movie, return results
+def search_movie(title)
+  headers = {
+    accept:              'application/json',
+    api_key:             ENV['TMDB_API_KEY'],
+    append_to_response:  'configuration'
+    content_type:        'json',
+    query:               title,
+  }
+
+  response      = RestClient.get "https://api.themoviedb.org/3/search/movie/", { params: headers }
+  json_response = JSON.parse(response)
+
+  return json_response
+end
+
+def get_movie_by_id(id)
+  headers = {
+    accept:  'application/json',
+    api_key: ENV['TMDB_API_KEY']
+  }
+
+  response      = RestClient.get "https://api.themoviedb.org/3/movie/#{id}", { params: headers }
   json_response = JSON.parse(response)
 
   return json_response
@@ -56,4 +80,14 @@ get '/search' do
   @results = search_movie(query)
 
   { :data => @results }.to_json
+end
+
+get '/movie/:id' do
+  id = params[:id]
+
+  @film_data = get_movie_by_id(id)
+
+  { :data => @film_data }.to_json
+
+  haml :movie
 end
