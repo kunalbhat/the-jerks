a3acfd704fe40f79c525803eb1f54e77492219d6
Kunal Bhat <kunalbhat@gmail.com>
2016-08-05 08:46:49 -0500

    Add a User class

---
+++ models/user.rb
@@ -0,0 +1,11 @@
+class User
+  include DataMapper::Resource
+  include BCrypt
+
+  property :id, Serial, :key => true
+  property :username, String, :length => 3..50
+  property :password, BCryptHash
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
