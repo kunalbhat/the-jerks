e1606ba94e43c0437913a73ec93930401fd3f011
Kunal Bhat <kunalbhat@gmail.com>
2016-08-06 10:11:17 -0500

    Add columns to Proposal model for votes

--- models/proposal.rb
+++ models/proposal.rb
@@ -8,6 +8,11 @@ class Proposal
   property :username, String, :length => 1
   property :proposal_text, Text
   property :date, DateTime
+  property :j_vote, Boolean, :default => false
+  property :e_vote, Boolean, :default => false
+  property :r_vote, Boolean, :default => false
+  property :k_vote, Boolean, :default => false
+  property :s_vote, Boolean, :default => false
 end
 
 DataMapper.finalize
